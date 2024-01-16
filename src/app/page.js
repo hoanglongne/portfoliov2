2eadfdeeeedaa8026d70cdaca7c50566689d5ba2
hoanglongne <73406617+hoanglongne@users.noreply.github.com>
2024-01-16 22:37:40 +0700

    Refactor page.js layout and styling

--- src/app/page.js
+++ src/app/page.js
@@ -8,14 +8,16 @@ export default function Home() {
   return (
     <RootLayout>
       <Navbar />
-      <div className='flex justify-center flex-col md:block'>
-        <h1 className='text-[16vw] leading-[90%] tracking-[-0.275rem] font-semibold ml-[20px] mt-[45px] md:mt-[100px] md:ml-[112px]'>
+      <div className='relative flex flex-col md:block'>
+        <h1 className='text-[16vw] leading-[90%] tracking-[-0.275rem] font-semibold ml-[20px] mt-[45px] md:mt-[100px] md:ml-[60px] lg:ml-[112px]'>
           Websites& <br />Branding
         </h1>
-        <div className='w-full flex justify-center mt-8'>
-          <div className='inline-flex rounded-full bg-gray-100 p-8 flex-row gap-2 items-center space-y-2'>
-          SCROLL DOWN
-          <Image src={ScrollDown} alt="Scrolldown image" width={15} height={15} />
+        <div className='w-full absolute right-0 bottom-0 flex justify-end mt-8'>
+          <div className='inline-flex rounded-full bg-gray-100 p-5 md:p-8 pr-15 flex-row gap-2 items-center'>
+            <p className='hidden md:block'>
+              SCROLL DOWN
+            </p>
+              <Image src={ScrollDown} alt="Scrolldown image" width={15} height={15} />
           </div>
         </div>
       </div>
