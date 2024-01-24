a6ed02f526071da02d8153fcb3634c21a7ae18e8
hoanglongne <73406617+hoanglongne@users.noreply.github.com>
2024-01-24 23:27:20 +0700

    Refactor page.js and add projects

---
+++ public/project.js
@@ -0,0 +1,24 @@
+const projects = [
+    {
+      name: 'Project 1',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      image: '/Project1.png'
+    },
+    {
+      name: 'Project 2',
+      description: 'Lorem ipsum do lor sit amet, consectetur adipiscing elit.lor sit amet, consectetur adipiscing elit. lor sit amet, consectetur adipiscing elit. lor sit amet, consectetur adipiscing elit. lor sit amet, consectetur adipiscing elit. lor sit amet, consectetur adipiscing elit. lor sit amet, consectetur adipiscing elit. lor sit amet, consectetur adipiscing elit. ',
+      image: '/Project2.png',
+    },
+    {
+      name: 'Project 3',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      image: '/Project3.png'
+    },
+    {
+      name: 'Project 4',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      image: '/Project4.png',
+    }
+  ]
+  
+export default projects;
--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,7 @@
 import RootLayout from './layout'
 import Navbar from './components/Navbar'
-import ScrollDown from '../../public/ScrollDown.svg'
 import Image from 'next/image';
-
+import projects from '../../public/project.js'
 export default function Home() {
 
   return (
@@ -18,16 +17,17 @@ export default function Home() {
               <p className='hidden lg:block'>
                 SCROLL DOWN
               </p>
-              <Image src={ScrollDown} alt="Scrolldown image" width={15} height={15} />
+              <Image src="/ScrollDown.svg" alt="Scrolldown image" width={15} height={15} />
             </div>
           </div>
         </div>
-      
-        <div className='flex justify-between gap-[10vw] md:gap-[20vw] lg:gap-[40vw] md:px-12'>
+
+        <div className='flex flex-col md:flex-row justify-between gap-[10vw] md:gap-[20vw] lg:gap-[40vw] md:px-12'>
           <div className='flex flex-col justify-center gap-2'>
             <h4 className='text-xl lg:text-2xl'>Contact Me</h4>
             <p className='text-sm lg:text-lg'>ngytrhoanglong61@gmail.com</p>
           </div>
+          <hr className='border-t-2 border-gray-100 md:hidden' />
           <p className='text-sm md:text-base lg:text-lg'>
             Enthusiastic Full-Stack Dev | 2+ years building web apps. Always learning, thriving on cutting-edge tech!  Eyeing a pivot to DevOps soon & currently exploring the exciting world of blockchain projects.
           </p>
@@ -36,18 +36,21 @@ export default function Home() {
         <div className='md:px-12 flex relative lg:mt-10'>
           <div className='flex flex-col gap-5'>
             <h4 className='text-4xl'>Projects</h4>
-            <p className='w-[30vw]'>Forget the ordinary, embrace the extraordinary. My passion for cutting-edge design will forge a brand that stands out from the crowd and sets your imagination ablaze.</p> 
-          </div>            
-          <div className='absolute right-0 bottom-0'>
+            <p className='w-[70vw] md:w-[50vw] lg:w-[30vw]'>Forget the ordinary, embrace the extraordinary. My passion for cutting-edge design will forge a brand that stands out from the crowd and sets your imagination ablaze.</p>
+          </div>
+          <div className='absolute right-0 top-0 md:bottom-0 md:top-auto'>
             <a href='#' className='text-sm lg:pr-12'>View All</a>
           </div>
         </div>
 
         <div class="grid grid-cols-1 md:grid-cols-2 gap-4 mt-[-50px] md:px-12">
-          <div className='w-auto'></div>
-          <div className='w-auto'></div>
-          <div className='w-auto'></div>
-          <div className='w-auto'></div>
+          {projects.map((project, index) => (
+            <div key={index} className='w-auto flex flex-col gap-3 mb-10'>
+              <Image src={project.image} alt={project.name} layout='responsive' width={500} height={300} />
+              <h2 className='font-semibold text-xl pl-5'>{project.name}</h2>
+              <p className='pl-5'>{project.description}</p>
+            </div>
+          ))}
         </div>
       </div>
     </RootLayout>
